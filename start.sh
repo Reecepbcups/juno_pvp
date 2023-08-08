6c3230c185d2644f7e1803269ba31bb25b0c75c4
Reece Williams <reecepbcups@gmail.com>
2023-08-08 20:09:42 +0200

    1.20 + duels patches

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 # TODO: Use https://github.com/Reecepbcups/minecraft-panel
 
-server=paper-1.18.2-388.jar
+# server=paper-1.18.2-388.jar
+server=paper-1.20.1-111.jar
 java -Xmx8024M -Xms8024M -jar $server nogui
