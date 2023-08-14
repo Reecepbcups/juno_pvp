f80c4386f481f4cab5168cfe03cf9034e357dff4
Reece Williams <reecepbcups@gmail.com>
2023-08-14 20:43:20 +0200

    Add spark

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
-# TODO: Use https://github.com/Reecepbcups/minecraft-panel
+# TODO: Use https://github.com/Reecepbcups/minecraft-panel?
+# java -Xmx8024M -Xms8024M -jar $server nogui
 
 # server=paper-1.18.2-388.jar
 server=paper-1.20.1-111.jar
-# java -Xmx8024M -Xms8024M -jar $server nogui
+MAXRAM=8024M 
+MINRAM=8024M 
 
-while true
-do
-    java -Xmx8024M -Xms8024M -jar $server nogui
+while [ true ]; do
+    java -Xmx$MAXRAM -Xms$MINRAM -jar $server nogui
     echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
     echo "Rebooting in:"
     for i in 5 4 3 2 1; do
